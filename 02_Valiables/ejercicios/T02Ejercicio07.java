d442a759d493a3acfea99dff5981d0e6bfc7c2c7
Alejandro-BR <barrionuevorosadoalejandro@gmail.com>
2023-10-26 17:55:01 +0200

    Ejercicio 7 tema 2

---
+++ 02_Valiables/ejercicios/T02Ejercicio07.java
@@ -0,0 +1,23 @@
+/**
+ * Ejercicio 7: Escribe un programa que declare variables de tipo char y tipo string. 
+ * Intenta mostrarlas por pantalla todas juntas en la misma line y con una sola sentencia de java (con un solo println) ¿es posible?
+ * 
+ * @author Alejandro Barrionuevo Rosado
+ */
+public class T02Ejercicio07 {
+  public static void main(String[] args) {
+    char letraAM = 'A';
+    char letraL = 'l';
+    char letraE = 'e';
+    char letraJ = 'j';
+    char letraA = 'a';
+    char letraN = 'n';
+    char letraD = 'd';
+    char letraR = 'r';
+    char letraO = 'o';
+    String apellidos = "Barrionuevo Rosado";
+
+    System.out.println(letraAM + "" +  letraL + "" + letraE + "" + letraJ +  "" + letraA + "" + letraN + "" + letraD + "" + "" + letraR + "" +  letraO + " " + apellidos );
+
+  }
+}
