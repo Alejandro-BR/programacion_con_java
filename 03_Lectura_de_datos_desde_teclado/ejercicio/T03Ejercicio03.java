c47d52d9923d002cb219eb2bb7a62d34bbe860b3
Alejandro-BR <barrionuevorosadoalejandro@gmail.com>
2023-10-28 15:20:06 +0200

    Ejercicio 3 tema 3

---
+++ 03_Lectura_de_datos_desde_teclado/ejercicio/T03Ejercicio03.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+/**
+ * Ejercicio 3: Realiza un conversor de psetas a euros. La cantidad de psetas que se requiere convertir deber ser introducida por teclado
+ * 
+ * @author Alejandro Barrionuevo Rosado
+ */
+public class T03Ejercicio03 {
+  public static void main(String[] args) {
+    // 166.386 pesetas = 1 euro
+    
+    System.out.println(""); //SALTO DE LINEA
+    System.out.println("CONVERTIDOR DE PESETAS A EUROS: ");
+    System.out.println(""); //SALTO DE LINEA
+
+    Scanner sc = new Scanner(System.in); //Creamos un objetos de la clase Scanner, llamado sc"
+    
+    System.out.print("Introduce las pesetas que quieres convertir: ");
+    double pesetas = sc.nextInt();
+    
+    sc.close(); //Cierre del Scanner; 
+
+    double euros = pesetas / 166.386;
+
+    System.out.println(""); //Espacios
+    System .out.printf(" %.0f", pesetas ); 
+    System.out.print (" pesetas = ");
+    System.out.printf(" %.2f", euros ); 
+    System.out.println(" euros.");
+    System.out.println(""); //Espacios
+  }
+}
