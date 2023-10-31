3e0419a8472aa0175b2db54dfff01cb8a791e6cc
Alejandro-BR <barrionuevorosadoalejandro@gmail.com>
2023-10-31 15:58:05 +0100

    Tema 4

---
+++ 04_Sentencia_condicional/ejemplos/Ejemplo01.java
@@ -0,0 +1,28 @@
+/**
+ * Escribe un programa que solicite por teclado un valor tipo numerico entero positivo y, hecho esto, muestre por pantalla
+ * un mensaje que indique si ha aprobado o no.
+ * 
+ * @author Alejandro Barrionuevo Rosado
+ */
+
+import java.util.Scanner;
+
+/**
+ * Ejemplo01
+ */
+public class Ejemplo01 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int nota;
+
+    System.out.print("¿Que nota has sacado en el ultimo examen?: ");
+    nota = sc.nextInt();
+     sc.close(); //Cierre del Scanner; 
+
+    if (nota >= 5) {
+      System.out.println("¡¡¡ENHORABUENA!!!");
+    }else{
+      System.out.println("Estudia mas");
+    }
+  }
+}
