8552fa7e6d58f6288e03ff9bc0faf77741e89da3
Alejandro-BR <barrionuevorosadoalejandro@gmail.com>
2023-10-31 17:21:53 +0100

    Ejercicio 3

--- 04_Sentencia_condicional/ejemplos/Ejercicio03Ejm.java
+++ 04_Sentencia_condicional/ejemplos/Ejercicio03Ejm.java
@@ -6,8 +6,23 @@
  * 
  * @author Alejandro Barrionuevo Rosado
  */
+import java.util.Scanner; 
 public class Ejercicio03Ejm {
   public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
     
+    System.out.println("      MENU:");
+    System.out.println("*****************");
+    System.out.println("1. Cuadrado");
+    System.out.println("2. Rectangulo");
+    System.out.println("3. Triangulo");
+    System.out.println("");
+    System.out.println("Elige el modo de lo que quieras obtener su area:");
+
+    int modo = sc.nextInt();
+    sc.close(); 
+
+
   }
 }
