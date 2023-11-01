194373b13902f2437c45cf552b3668a127b4d239
Alejandro-BR <barrionuevorosadoalejandro@gmail.com>
2023-11-01 19:49:04 +0100

    Ejercicio 5 tema 3 (Comenzado)

---
+++ 03_Lectura_de_datos_desde_teclado/ejercicios/T03Ejercicio05.java
@@ -0,0 +1,10 @@
+/**
+ * Ejercicio 5: Escribe un programa que calcule el area de un rectangulo.
+ * 
+ * @author Alejandro Barrionuevo Rosado
+ */
+public class T03Ejercicio05 {
+  public static void main(String[] args) {
+    
+  }
+}
