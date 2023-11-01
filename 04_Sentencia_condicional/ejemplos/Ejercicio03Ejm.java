c263999b572b6d70e64a9ae23ae3ad4b50043e8b
Alejandro-BR <barrionuevorosadoalejandro@gmail.com>
2023-11-01 19:59:37 +0100

    Ejercicio 3 de los ejemplos del tema 4

--- 04_Sentencia_condicional/ejemplos/Ejercicio03Ejm.java
+++ 04_Sentencia_condicional/ejemplos/Ejercicio03Ejm.java
@@ -11,18 +11,52 @@ public class Ejercicio03Ejm {
   public static void main(String[] args) {
 
     Scanner sc = new Scanner(System.in);
-    
+    System.out.println(""); //SALTO DE PAGINA 
     System.out.println("      MENU:");
     System.out.println("*****************");
     System.out.println("1. Cuadrado");
     System.out.println("2. Rectangulo");
     System.out.println("3. Triangulo");
     System.out.println("");
-    System.out.println("Elige el modo de lo que quieras obtener su area:");
+    System.out.print("Elige el modo de lo que quieras obtener su area: ");
 
     int modo = sc.nextInt();
-    sc.close(); 
 
-
+    System.out.println(""); //SALTO DE PAGINA
+    
+    switch (modo) {
+      case 1:
+        System.out.print("Introduce la altura del cuadrado: ");
+        int alturaC = sc.nextInt();
+        System.out.print("Introduce el ancho del cuadrado: ");
+        int anchoC = sc.nextInt();
+        int areaC = anchoC * alturaC;
+        System.out.println(""); //SALTO DE PAGINA
+        System.out.println("La area del cuadrado es " + areaC);
+        break;
+      case 2:
+        System.out.print("Introduce la altura del rectangulo: ");
+        int alturaR = sc.nextInt();
+        System.out.print("Introduce el ancho del rectangulo: ");
+        int anchoR = sc.nextInt();
+        int areaR = anchoR * alturaR;
+        System.out.println(""); //SALTO DE PAGINA
+        System.out.println("La area del rectangulo es " + areaR);
+        break;
+      case 3:
+        System.out.print("Introduce la altura del triangulo: ");
+        int alturaT = sc.nextInt();
+        System.out.print("Introduce la base del triangulo: ");
+        int baseT = sc.nextInt();
+        int areaT = baseT * alturaT;
+        System.out.println(""); //SALTO DE PAGINA
+        System.out.println("La area del triangulo es " + areaT);
+        break;
+      default:
+        System.out.println("Introduce un valor correspondiente al menu porfavor.");
+        break;
+    }
+    sc.close(); //Cierre del Scanner 
+    System.out.println(""); //SALTO DE PAGINA 
   }
 }
