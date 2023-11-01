cde803cebccd6b78cb00326190b05e579a2be1ba
Alejandro-BR <barrionuevorosadoalejandro@gmail.com>
2023-11-01 20:09:50 +0100

    Ejercicio 5 tema 3

--- 03_Lectura_de_datos_desde_teclado/ejercicios/T03Ejercicio05.java
+++ 03_Lectura_de_datos_desde_teclado/ejercicios/T03Ejercicio05.java
@@ -3,8 +3,18 @@
  * 
  * @author Alejandro Barrionuevo Rosado
  */
+import java.util.Scanner; 
 public class T03Ejercicio05 {
-  public static void main(String[] args) {
-    
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);  //Creamos un objetos de la clase Scanner, llamado sc 
+    System.out.print("Introduce la altura del rectangulo: ");
+    int alturaR = sc.nextInt();
+    System.out.print("Introduce el ancho del rectangulo: ");
+    int anchoR = sc.nextInt();
+    int areaR = anchoR * alturaR;
+    System.out.println(""); //SALTO DE PAGINA
+    System.out.println("La area del rectangulo es " + areaR);
+    sc.close(); //Cierre del Scanner 
+    System.out.println(""); //SALTO DE PAGINA  
   }
 }
