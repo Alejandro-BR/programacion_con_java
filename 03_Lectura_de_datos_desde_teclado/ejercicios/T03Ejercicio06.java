83b5ac9666ee7783f9d7b9aee0bded34d381f38f
Alejandro-BR <barrionuevorosadoalejandro@gmail.com>
2023-11-01 20:42:56 +0100

    Ejercicio 6 tema 3 (Comenzado)

---
+++ 03_Lectura_de_datos_desde_teclado/ejercicios/T03Ejercicio06.java
@@ -0,0 +1,13 @@
+/**
+ * Ejercicio 6: Escribe un programa que calcule el area de un triangulo.
+ * 
+ * @author Alejandro Barrionuevo Rosado
+ */
+import java.util.Scanner; 
+public class T03Ejercicio06 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);  //Creamos un objetos de la clase Scanner, llamado sc 
+    
+    sc.close(); //Cierre del Scanner 
+  }
+}
