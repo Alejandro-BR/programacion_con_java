f4de49c9ba3974f8bdff2a200ba5dd9e173c41e6
Alejandro-BR <barrionuevorosadoalejandro@gmail.com>
2023-11-02 13:21:38 +0100

    Ejercicio 9 tema 3 (Comienzo)

---
+++ 03_Lectura_de_datos_desde_teclado/ejercicios/T03Ejercicio09.java
@@ -0,0 +1,11 @@
+/**
+ * Ejercicio 9: Escribe un programa que calcule el volumen de un cono según la fórmula V = 1/3 * pi * r2 * h
+ * 
+ * @author Alejandro Barrionuevo Rosado
+ */
+public class T03Ejercicio09 {
+  public static void main(String[] args) {
+    double PI = Math. PI;
+    System.out.println(PI);
+  }
+}
