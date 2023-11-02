1d338cccaf1e47e7ec9e4a98f99708ecae522350
Alejandro-BR <barrionuevorosadoalejandro@gmail.com>
2023-11-02 12:30:10 +0100

    Ejercicio 7 tema 3

---
+++ 03_Lectura_de_datos_desde_teclado/ejercicios/T03Ejercicio07.java
@@ -0,0 +1,85 @@
+/**
+ * Ejercicio 7: Escribe un programa que calcule el total de una factura a partir de la base imponible.
+ * 
+ * @author Alejandro Barrionuevo Rosado
+ */
+import java.util.Scanner;
+
+public class T03Ejercicio07 {
+  public static void main(String[] args) {
+    //el porcentaje de IVA en españa es de 21% es decir multiplicar por 0.21
+    double baseImponible;
+    double iva = 0.21;
+    double ivaProducto;
+    double total;
+    double pago;
+    double cambio;
+    int dia;
+    int mes;
+    int año;
+    
+    System.out.println(""); //SALTO DE LINEA 
+    System.out.println("TIENDA:");
+    System.out.println(""); //SALTO DE LINEA
+
+    Scanner sc = new Scanner(System.in);  //Creamos un objetos de la clase Scanner, llamado sc 
+
+    System.out.print("Precio del producto: ");
+    baseImponible = sc.nextDouble();
+
+    System.out.println(""); //SALTO DE LINEA 
+
+    System.out.print("Introduce el dia: ");
+    dia = sc.nextInt();
+    System.out.print("Introduce el mes: ");
+    mes = sc.nextInt();
+    System.out.print("Introduce el año: ");
+    año = sc.nextInt();
+
+    System.out.println(""); //SALTO DE LINEA 
+
+    System.out.print("Introduce cuanto pago el cliente: ");
+    pago = sc.nextDouble();
+
+    System.out.println(""); //SALTO DE LINEA 
+    System.out.println(""); //SALTO DE LINEA 
+
+    sc.close(); //Cierre del Scanner 
+
+    ivaProducto = baseImponible * iva;
+    total = ivaProducto + baseImponible;
+    cambio = pago - total;
+
+    //System.out.println(euros + "€  --> " + pesetas + " pta");
+    //System.out.printf("%.2f € --> %d pta\n", euros + pesetas);
+    
+    System.out.println(""); //Salto de linea
+    System.out.println("*****************************");
+    System.out.println("*                           *");
+    System.out.println("* Ticket de compra          *");
+    System.out.println("*                           *");
+    System.out.println("*****************************");
+    System.out.println("*                           *");
+    System.out.println("* Fecha: "+ dia + "/" + mes + "/" + año + "           *");
+    System.out.println("*                           *");
+    System.out.println("*****************************");
+    System.out.println("*                           *");
+    System.out.printf("* Base imponible %8.2f € *\n", baseImponible);
+    System.out.println("*                           *");
+    System.out.println("* IVA                " + iva + " % *");
+    System.out.println("*                           *");
+    System.out.printf("* IVA Producto   %8.2f € *\n", ivaProducto);
+    System.out.println("*                           *");
+    System.out.println("*****************************");
+    System.out.println("*                           *");
+    System.out.printf("* TOTAL          %8.2f € *\n", total);
+    System.out.printf("* PAGO           %8.2f € *\n", pago);
+    System.out.printf("* CAMBIO         %8.2f € *\n", cambio);
+    System.out.println("*                           *");
+    System.out.println("*****************************");
+    System.out.println(""); //Salto de linea
+    
+
+
+  }
+}
