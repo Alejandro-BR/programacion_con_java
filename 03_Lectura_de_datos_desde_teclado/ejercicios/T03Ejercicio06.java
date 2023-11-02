bfcb5bba572073174a32f55bd9f1053578b460e3
Alejandro-BR <barrionuevorosadoalejandro@gmail.com>
2023-11-02 11:28:09 +0100

    Ejercicio 6 tema 3

--- 03_Lectura_de_datos_desde_teclado/ejercicios/T03Ejercicio06.java
+++ 03_Lectura_de_datos_desde_teclado/ejercicios/T03Ejercicio06.java
@@ -6,8 +6,25 @@
 import java.util.Scanner; 
 public class T03Ejercicio06 {
   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);  //Creamos un objetos de la clase Scanner, llamado sc 
+
+    System.out.println(""); //SALTO DE LINEA 
+    System.out.println("AREA DEL TRIANGULO: ");
     
+    Scanner sc = new Scanner(System.in);  //Creamos un objetos de la clase Scanner, llamado sc 
+
+    System.out.println(""); //SALTO DE LINEA
+    System.out.print("Introduce la altura del triangulo: ");
+    double altura = sc.nextDouble();
+    System.out.print("Introduce la base del triangulo: ");
+    double base = sc.nextDouble();
+    System.out.println(""); //SALTO DE LINEA
+
     sc.close(); //Cierre del Scanner 
+
+    double area = base * altura;
+
+    System.out.println("El area del triangulo es " + area);
+    System.out.println(""); //SALTO DE LINEA
+
   }
 }
