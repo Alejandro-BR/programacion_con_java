e94fa6067ca26eac9afa46d300192a34447d4eb8
Alejandro-BR <barrionuevorosadoalejandro@gmail.com>
2023-11-02 21:27:10 +0100

    Ejercicio 9 tema 3

--- 03_Lectura_de_datos_desde_teclado/ejercicios/T03Ejercicio09.java
+++ 03_Lectura_de_datos_desde_teclado/ejercicios/T03Ejercicio09.java
@@ -3,9 +3,30 @@
  * 
  * @author Alejandro Barrionuevo Rosado
  */
+import java.util.Scanner; 
 public class T03Ejercicio09 {
   public static void main(String[] args) {
+  
     double PI = Math. PI;
-    System.out.println(PI);
+    double div = 0.33; //1/3
+    
+    System.out.println(""); //SALTO DE LINEA 
+    System.out.println("Calcule el volumen de un cono:");
+    System.out.println(""); //SALTO DE LINEA 
+
+    Scanner sc = new Scanner(System.in);  //Creamos un objetos de la clase Scanner, llamado sc 
+
+    System.out.print("Introduce la r: ");
+    double r = sc.nextDouble();
+
+    System.out.print("Introduce la h: ");
+    double h = sc.nextDouble();
+
+    sc.close(); //Cierre del Scanner 
+
+    double v = (div * PI * (r * 2) * h);
+
+    System.out.printf("V = 1/3 * PI * r2 * h =  %4.2f \n", v);
+    System.out.println(""); //SALTO DE LINEA 
   }
 }
