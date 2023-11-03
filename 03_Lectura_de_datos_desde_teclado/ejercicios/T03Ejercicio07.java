4eca1c62bfe0b5113d1dda41ea810ead3f2bc08e
Alejandro-BR <barrionuevorosadoalejandro@gmail.com>
2023-11-03 16:53:08 +0100

    Ejercicio 7 tema 3 comentarios

--- 03_Lectura_de_datos_desde_teclado/ejercicios/T03Ejercicio07.java
+++ 03_Lectura_de_datos_desde_teclado/ejercicios/T03Ejercicio07.java
@@ -79,7 +79,19 @@ public class T03Ejercicio07 {
     System.out.println("*****************************");
     System.out.println(""); //Salto de linea
     
+    //Para poner el caracter % por pantalla hay que poner %%
 
+    // Scanner sc = new Scanner(System.in);
+    // double baseImponible;
+    
+    // System.out.print("Por favor, introduzca la base imponible (precio del artículo sin IVA): ");
+    // baseImponible = sc.nextDouble();
 
+    // System.out.printf("\nBase imponible %8.2f €\n", baseImponible);
+    // System.out.printf("IVA (21%%)      %8.2f €\n", (baseImponible * 0.21));   //Para imprimir el carácter %, lo debemos escapar utilizando %%
+    // System.out.printf("-------------------------\n");
+    // System.out.printf("Total          %8.2f €\n", (baseImponible * 1.21));
+
+    // sc.close();
   }
-}
+  }
