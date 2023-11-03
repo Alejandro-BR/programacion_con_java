a18f114ae1ae2f08ed1f230a4f18b21cf51d247a
Alejandro-BR <barrionuevorosadoalejandro@gmail.com>
2023-11-03 17:07:10 +0100

    Ejercicio 9 correccion en la formula

--- 03_Lectura_de_datos_desde_teclado/ejercicios/T03Ejercicio09.java
+++ 03_Lectura_de_datos_desde_teclado/ejercicios/T03Ejercicio09.java
@@ -7,8 +7,7 @@ import java.util.Scanner;
 public class T03Ejercicio09 {
   public static void main(String[] args) {
   
-    final double PI = Math. PI; //constante 
-    double div = 0.33; //1/3
+    final double PI = Math.PI; //constante 
     
     System.out.println(""); //SALTO DE LINEA 
     System.out.println("Calcule el volumen de un cono:");
@@ -16,15 +15,15 @@ public class T03Ejercicio09 {
 
     Scanner sc = new Scanner(System.in);  //Creamos un objetos de la clase Scanner, llamado sc 
 
-    System.out.print("Introduce la r: ");
+    System.out.print("Introduce el radio (cm): ");
     double r = sc.nextDouble();
 
-    System.out.print("Introduce la h: ");
+    System.out.print("Introduce la altura (cm): ");
     double h = sc.nextDouble();
 
     sc.close(); //Cierre del Scanner 
 
-    double v = (div * PI * (r * 2) * h);
+    double v = ( PI * Math.pow(r, 2) * h) /3; //volumen    //Math.pow(a, b) Es un numero a elevado a b
 
     System.out.printf("V = 1/3 * PI * r2 * h =  %4.2f \n", v);
     System.out.println(""); //SALTO DE LINEA 
