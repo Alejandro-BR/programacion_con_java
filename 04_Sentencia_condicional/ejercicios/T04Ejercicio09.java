6986dd7fd97feaf8f69472060dc075a8a94418f6
Alejandro-BR <barrionuevorosadoalejandro@gmail.com>
2023-11-09 20:37:26 +0100

    Ejercicio 9 tema 4 me da fallo

--- 04_Sentencia_condicional/ejercicios/T04Ejercicio09.java
+++ 04_Sentencia_condicional/ejercicios/T04Ejercicio09.java
@@ -7,8 +7,26 @@
 import java.util.Scanner; 
 public class T04Ejercicio09 {
   public static void main(String[] args) {
+    System.out.println(""); //SALTO DE LINEA
+
     Scanner sc = new Scanner(System.in);  //Creamos un objetos de la clase Scanner, llamado sc 
     System.out.print("Introduce la a: ");
+    double a = sc.nextDouble();
+    System.out.print("Introduce la b: ");
+    double b = sc.nextDouble();
+    System.out.print("Introduce la c: ");
+    double c = sc.nextDouble();
     sc.close(); //Cierre del Scanner 
+
+    System.out.println(""); //SALTO DE LINEA 
+
+    if (a != 0) {
+      double x = ((b * b) - 4 * a * c);
+      System.out.printf("x = %1.1f",x);
+    } else{
+      System.out.println("Esa ecuacion no tiene solución real.");
+    }
+
+    System.out.println(""); //SALTO DE LINEA
   }
 }
