db52214fa344a39dce55580155f1c2e09e498f23
Alejandro-BR <barrionuevorosadoalejandro@gmail.com>
2023-11-09 20:11:25 +0100

    Ejercicio 9 tema 4 comienzo

---
+++ 04_Sentencia_condicional/ejercicios/T04Ejercicio09.java
@@ -0,0 +1,14 @@
+/**
+ * Ejercicio 9: Realiza un programa que resuelva una ecuación de segundo grado (del tipo
+ * ax2 + bx + c = 0).
+ * 
+ * @author Alejandro Barrionuevo Rosado
+ */
+import java.util.Scanner; 
+public class T04Ejercicio09 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);  //Creamos un objetos de la clase Scanner, llamado sc 
+    System.out.print("Introduce la a: ");
+    sc.close(); //Cierre del Scanner 
+  }
+}
---
+++ 04_Sentencia_condicional/ejercicios/T04Ejercicio18.java
@@ -0,0 +1,11 @@
+/**
+ * Ejercicio 18: Escribe un programa que diga cuál es la primera cifra de un número entero
+ * introducido por teclado. Se permiten números de hasta 5 cifras.
+ * 
+ * @author Alejandro Barrionuevo Rosado
+ */
+public class T04Ejercicio18 {
+  public static void main(String[] args) {
+    
+  }
+}
