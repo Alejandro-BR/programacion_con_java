6d2a67216168b8341ac1eed8c9215b1326279480
Alejandro-BR <barrionuevorosadoalejandro@gmail.com>
2023-11-09 20:18:10 +0100

    Ejercicio 18 tema 4

--- 04_Sentencia_condicional/ejercicios/T04Ejercicio18.java
+++ 04_Sentencia_condicional/ejercicios/T04Ejercicio18.java
@@ -4,8 +4,42 @@
  * 
  * @author Alejandro Barrionuevo Rosado
  */
+import java.util.Scanner; 
 public class T04Ejercicio18 {
   public static void main(String[] args) {
-    
+    int numero;
+    int primera = 0 ; //Aqui almaceno la primera cifra del numero
+    Scanner sc = new Scanner(System.in);  //Creamos un objetos de la clase Scanner, llamado sc 
+    System.out.print("Por favor, introduce un numero entero positivo (5 cifras como maximo0): ");
+    numero = sc.nextInt();
+    sc.close(); //Cierre del Scanner 
+
+    //Numero de una cifra 1
+    if (numero < 10) {
+      primera = numero;
+    }
+
+    //Numero de 2 cifras
+    if ((numero >= 10) && (numero < 100)) {
+      primera = numero / 10;
+    }
+
+    //Numero de 3 cifras
+    if ((numero >= 100) && (numero < 1000)) {
+      primera = numero / 100;
+    }
+
+
+    //Numero de 4 cifras
+    if ((numero >= 1000) && (numero < 10000)) {
+      primera = numero / 1000;
+    }
+
+    //Numero de 5 cifras
+    if ((numero >= 10000) && (numero < 100000)) {
+      primera = numero / 10000;
+    }
+
+    System.out.println("La primer cifra del numero " + numero + " es el " + primera);
   }
 }
