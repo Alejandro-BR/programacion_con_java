78b82cf4efbec4eb3c21880fa2ee3b14364331d9
Alejandro-BR <barrionuevorosadoalejandro@gmail.com>
2023-11-10 12:42:37 +0100

    Ejercicio 9 tema 4

--- 04_Sentencia_condicional/ejercicios/T04Ejercicio09.java
+++ 04_Sentencia_condicional/ejercicios/T04Ejercicio09.java
@@ -20,11 +20,47 @@ public class T04Ejercicio09 {
 
     System.out.println(""); //SALTO DE LINEA 
 
-    if (a != 0) {
-      double x = ((b * b) - 4 * a * c);
-      System.out.printf("x = %1.1f",x);
-    } else{
-      System.out.println("Esa ecuacion no tiene solución real.");
+    // if (a != 0) {
+    //   double x = ((b * b) - 4 * a * c);
+    //   System.out.printf("x = %1.1f",x);
+    // } else{
+    //   System.out.println("Esa ecuacion no tiene solución real.");
+    // }
+
+    double descriminante = Math.pow(b, 2) - (4*a*c);
+    
+    //ax2 + bx + c = 0 con a, b y c distintos de 0
+
+    if ((a != 0) && (b != 0) && (c != 0)) {
+      if (descriminante < 0) {
+        System.out.println("La ecuacion no tiene soluciones reales");
+      } else {
+        System.out.println("x1 = " + (-b + Math.sqrt(descriminante))/(2 * a));
+        System.out.println("x2 = " + (-b - Math.sqrt(descriminante))/(2 * a));
+      }
+    }
+
+    //0x2 + 0x + 0 = 0
+
+    if ((a == 0) && (b == 0) && (c == 0)) {
+      System.out.println("La ecuacion tiene infinitas soluciones.");
+    }
+
+    //0x2 + 0x + c = 0    con c distinto de 0
+    if ((a == 0) && (b == 0) && (c != 0)) {
+      System.out.println("La ecuacion no tiene solucion.");
+      
+    }
+
+    //sin termino independiente (c = 0)
+    if ((a != 0) && (b != 0) && (c == 0)) {
+      System.out.println("x1 = 0");
+      System.out.println("x2 = " + (-b/a));
+    }
+
+    //0x2 + bx + c = 0  con b y c distinto de 0
+    if ((a == 0) && (b != 0) && (c != 0)) {
+      System.out.println("x1 = x2 " + (-c / b));
     }
 
     System.out.println(""); //SALTO DE LINEA
