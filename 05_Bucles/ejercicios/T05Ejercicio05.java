0680f1660a03794abd7c3a700a1c1af630518ef4
Alejandro-BR <barrionuevorosadoalejandro@gmail.com>
2023-11-15 19:47:15 +0100

    Ejercicio 5 y 6 tema 5

---
+++ 05_Bucles/ejercicios/T05Ejercicio05.java
@@ -0,0 +1,16 @@
+/**
+ * Ejercicio 5: Muestra los números del 320 al 160, contando de 20 en 20 hacia atrás utilizando
+ * un bucle while.
+ * 
+ * @author Alejandro Barrionuevo Rosado
+ */
+public class T05Ejercicio05 {
+  public static void main(String[] args) {
+    int i = 320;
+    while (i >= 160) {
+      System.out.println(i);
+      i = i - 20;  // i -= 20
+    }
+
+  }
+}
---
+++ 05_Bucles/ejercicios/T05Ejercicio06.java
@@ -0,0 +1,15 @@
+/**
+ * Ejercicio 6: Muestra los números del 320 al 160, contando de 20 en 20 utilizando un bucle
+ * do-while.
+ * 
+ * @author Alejandro Barrionuevo Rosado
+ */
+public class T05Ejercicio06 {
+  public static void main(String[] args) {
+    int i = 320;
+    do {
+      System.out.println(i);
+      i -= 20;
+    } while (i >= 160);
+  }
+}
