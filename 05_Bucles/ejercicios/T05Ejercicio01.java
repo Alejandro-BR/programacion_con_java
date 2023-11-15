2d85b278ff420e18169c99c67fefb82058f4e904
Alejandro-BR <barrionuevorosadoalejandro@gmail.com>
2023-11-15 17:31:41 +0100

    Ejercicios tema 5

---
+++ 05_Bucles/ejercicios/T05Ejercicio01.java
@@ -0,0 +1,21 @@
+/**
+ * Ejercicio 1: Muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle for.
+ * 
+ * @author Alejandro Barrionuevo Rosado
+ */
+public class T05Ejercicio01 {
+  public static void main(String[] args) {
+    int multiplos = 0;
+    System.out.println(""); //SALTO DE LINEA 
+    System.out.println("Numeros multiplos de 5:");
+    System.out.println(""); //SALTO DE LINEA 
+    for (int i = 0; i <= 100; i++){
+      System.out.println(i);
+      multiplos = 5 / i;
+      if (multiplos == 0) {
+        System.out.println("Es multiplo de 5 el numero " + i);
+        System.out.println(""); //SALTO DE LINEA 
+      }
+    }
+  }
+}
