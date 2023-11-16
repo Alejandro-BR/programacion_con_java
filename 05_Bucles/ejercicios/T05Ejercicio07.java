fca8356aedbe2bdf023b66ee6ba9964dc5bdcda4
Alejandro-BR <barrionuevorosadoalejandro@gmail.com>
2023-11-16 13:29:36 +0100

    Ejercicio 7 tema 5 terminado

--- 05_Bucles/ejercicios/T05Ejercicio07.java
+++ 05_Bucles/ejercicios/T05Ejercicio07.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 /**
  * Ejercicio 7: Realiza el control de acceso a una caja fuerte. La combinación será un
  * número de 4 cifras. El programa nos pedirá la combinación para abrirla. Si no
@@ -15,14 +13,22 @@ public class T05Ejercicio07 {
     int contraseñaIntroducida;
     int contraseña = 1234;
     int intentos = 4;
+
+    System.out.println(""); //SALTO DE LINEA 
+    
     Scanner sc = new Scanner(System.in);  //Creamos un objetos de la clase Scanner, llamado sc 
     do{
       System.out.print("Introduce la contraseña: ");
       contraseñaIntroducida = sc.nextInt();
-    } while ((contraseñaIntroducida != contraseña ) && (intentos >= 0));
+      intentos--;
+      if (contraseñaIntroducida == contraseña) {
+        System.out.println("Introduciste la contraseña correcta.");
+      } else {
+        System.out.println("Introduciste una contraseña incorrecta. Te quedan " + intentos + " intentos." );
+      }
+    } while ((contraseñaIntroducida != contraseña ) && (intentos > 0));
     sc.close(); //Cierre del Scanner 
-
-
-    //No esta terminado terminar en casa
+    
+    System.out.println(""); //SALTO DE LINEA 
   }
 }
