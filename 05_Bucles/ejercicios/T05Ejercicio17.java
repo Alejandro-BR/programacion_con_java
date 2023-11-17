1416d080281f55d1623d430cd40695bad51bb26c
Alejandro-BR <barrionuevorosadoalejandro@gmail.com>
2023-11-17 16:54:31 +0100

    Ejercicio 17 tema 5 arreglado

--- 05_Bucles/ejercicios/T05Ejercicio17.java
+++ 05_Bucles/ejercicios/T05Ejercicio17.java
@@ -8,24 +8,19 @@
 import java.util.Scanner; 
 public class T05Ejercicio17 {
   public static void main(String[] args) {
-    int numeroSumado;
-    int numeroSumado2;
     int suma = 0;
     int numero;
     System.out.println(""); //SALTO DE LINEA 
 
     Scanner sc = new Scanner(System.in);  //Creamos un objetos de la clase Scanner, llamado sc 
     do {
-      System.out.print("Introduce el numero entero: ");
+      System.out.print("Introduce el numero entero positivo: ");
       numero = sc.nextInt();
-    } while(numero < 0);
+    } while(numero <= 0);
 
-    sc.close(); //Cierre del Scanner
-    numeroSumado = numero; 
-    for (int i = 0; i <= 100; i++){
-      numeroSumado2 = numeroSumado;
-      numeroSumado++;
-      suma = numeroSumado + numeroSumado2;
+    sc.close(); //Cierre del Scanner 
+    for (int i = numero + 1; i <= numero + 100; i++){
+      suma += i;
     }
 
     System.out.println("Total = " + suma);
