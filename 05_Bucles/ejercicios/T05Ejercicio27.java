695048d4d716f02b68dc8c4bbec4c9a45a1546b9
Alejandro-BR <barrionuevorosadoalejandro@gmail.com>
2023-11-19 17:20:53 +0100

    Ejercicio 27 tema 5

--- 05_Bucles/ejercicios/T05Ejercicio27.java
+++ 05_Bucles/ejercicios/T05Ejercicio27.java
@@ -3,8 +3,35 @@
  * 
  * @author Alejandro Barrionuevo Rosado
  */
+import java.util.Scanner; 
 public class T05Ejercicio27 {
   public static void main(String[] args) {
-    
+    double resto;
+    int multiplos = 0;
+    int suma = 0;
+
+    System.out.println(""); //SALTO DE LINEA 
+    System.out.println("Del numero introducido por pantalla este programa dira los multiplos que hay desde el numero hasta 1:");
+    System.out.println(""); //SALTO DE LINEA 
+
+    Scanner sc = new Scanner(System.in);  //Creamos un objetos de la clase Scanner, llamado sc 
+    System.out.print("Introduce el numero: ");
+    int numero = sc.nextInt();
+    System.out.println(""); //SALTO DE LINEA 
+    sc.close(); //Cierre del Scanner 
+
+    for (int i = numero; i >= 1; i--){
+      resto = i % 3;
+      if (resto == 0) {
+        System.out.println(i);
+        multiplos++;
+        suma += i;
+      }
+    }
+
+    System.out.println(""); //SALTO DE LINEA 
+    System.out.printf("Hay %1d numeros multiplos de 3 entre el numero %2d y 1. \n",multiplos, numero); 
+    System.out.println("La suma de todos los multiplos de tres es: " + suma); 
+    System.out.println(""); //SALTO DE LINEA 
   }
 }
