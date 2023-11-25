70dfc684705713aac032c40157f2f6fc807a0b16
Alejandro-BR <barrionuevorosadoalejandro@gmail.com>
2023-11-25 12:08:59 +0100

    Ejercicio 32 tema 5

--- 05_Bucles/ejercicios/T05Ejercicio32.java
+++ 05_Bucles/ejercicios/T05Ejercicio32.java
@@ -23,31 +23,39 @@
  * Suma de los dígitos pares: 10
  * 
  * 
- * No me sale no entiendo como hacerlo mirar mas adelante o preguntar en clase
+ * 
  * 
  * @author Alejandro Barrionuevo Rosado
  */
 import java.util.Scanner; 
 public class T05Ejercicio32 {
   public static void main(String[] args) {
-    long numeroIntroducido = 0;
-    long ultimoDigito = 0;
-    int contador = 0;
-
-    Scanner sc = new Scanner(System.in);  //Creamos un objetos de la clase Scanner, llamado sc 
-    System.out.println(""); //SALTO DE LINEA 
     System.out.print("Por favor, introduzca un número entero positivo: ");
-    numeroIntroducido = sc.nextLong();
-    sc.close(); //Cierre del Scanner 
-    
-    ultimoDigito = numeroIntroducido;
+    long numeroIntroducido = Long.parseLong(System.console().readLine());
+    long numeroVolteado = 0;
+    int numeroCifras = 0;
+    int digito;
+    int sumaPares = 0;
 
-    while (ultimoDigito > 10) {
-      ultimoDigito = ultimoDigito - ( numeroIntroducido / 10);
+    //Al tener que mostrarlos de izquierda a derecha, mejor voltearemos el número como haciamos en el Ejercicio 25
+    //y de paso calculamos el número de cifras
+    while (numeroIntroducido > 0) {
+      numeroVolteado = (numeroIntroducido % 10) + (numeroVolteado * 10);
+      numeroIntroducido /= 10;
+      numeroCifras++;
     }
 
-    System.out.println(ultimoDigito);
-    System.out.println(contador);
-    System.out.println(""); //SALTO DE LINEA 
+    System.out.print("Dígitos pares: ");
+
+    for (int i = 0; i < numeroCifras; i++) {
+      digito = (int) (numeroVolteado % 10);
+      //Una vez tomada la cifra, vemos si es par haciendo la operacion MOD 2
+      if (digito % 2 == 0) {
+        System.out.print(digito + " ");   // Muestra los dígitos pares
+        sumaPares += digito;
+      }
+      numeroVolteado /= 10;   //Dividimos por 10 para obtener la siguiente cifra del número volteado
+    }
+    System.out.println("\nLa suma de los dígitos pares es " + sumaPares);
   }
 }
---
+++ 05_Bucles/ejercicios/T05Ejercicio37.java
@@ -0,0 +1,6 @@
+
+public class T05Ejercicio37 {
+  public static void main(String[] args) {
+    
+  }
+}
