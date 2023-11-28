73836cd251e5bb518879c371033f6c9912b2fa5b
Alejandro Barrionuevo Rosado <barrionuevorosadoalejandro@gmail.com>
2023-11-28 21:12:33 +0100

    Ejercicio 44 tema 5 comenzado

---
+++ 05_Bucles/ejercicios/T05Ejercico44.java
@@ -0,0 +1,29 @@
+/**
+ * Ejercicio 44: Escribe un programa que sea capaz de insertar un dígito dentro de un número
+ * indicando la posición. El nuevo dígito se colocará en la posición indicada y el
+ * resto de dígitos se desplazará hacia la derecha. Las posiciones se cuentan de
+ * izquierda a derecha empezando por el 1. Suponemos que el usuario introduce
+ * correctamente los datos. Se recomienda usar long en lugar de int ya que el
+ * primero admite números más largos.
+ * 
+ * Ejemplo:
+ * 
+ * Por favor, introduzca un número entero positivo: 406783
+ * Introduzca la posición donde quiere insertar: 3
+ * Introduzca el dígito que quiere insertar: 5
+ * El número resultante es 4056783.
+ * 
+ * @author Alejandro Barrionuevo Rosado
+ */
+public class T05Ejercico44 {
+  public static void main(String[] args) {
+    try {
+      
+    } catch (Exception e) {
+      System.out.println(""); //SALTO DE LINEA 
+      System.out.println("Error de mensaje: " + e.getMessage());
+      System.out.println("Clase de excepcion: " + e.getClass());
+      System.out.println(""); //SALTO DE LINEA 
+    }
+  }
+}
