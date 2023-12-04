06b42f79ddcc7a576dca26a80186501f9800e727
Alejandro Barrionuevo Rosado <barrionuevorosadoalejandro@gmail.com>
2023-12-04 17:11:41 +0100

    Ejercicio Histogramas tema 5 comentarios

--- 05_Bucles/ejercicios/T05Histograma2.java
+++ 05_Bucles/ejercicios/T05Histograma2.java
@@ -1,3 +1,4 @@
+
 /**
  * Ejercicio Hitograma: Debes crear un programa que pida una serie de números entre 0 y 9.
  * El programa seguirá admitiendo números hasta que se introduzca uno negativo. 
@@ -19,90 +20,81 @@
  * 
  * @author Alejandro Barrionuevo Rosado
  */
-import java.util.Scanner; 
+import java.util.Scanner;
+
 public class T05Histograma2 {
   public static void main(String[] args) {
     try {
-      //int numero;
-      //int sumaNumero = 0;
+      //Variables
+      int numeroIntroducido = 0;
       int digitos = 0;
       int numeroDigitos = 0;
       int numeroVolteado = 0;
-      int numeroIntroducido = 0;
       int copiaNumero = 0;
       int copiaNumeroDigitos = 0;
 
-      System.out.println(""); //SALTO DE LINEA 
+      System.out.println(""); // SALTO DE LINEA
       System.out.println("Introduzca numeros entre 0 y 9 y pulsa INTRO. Introduce un numero negativo si desea parar.");
-      Scanner sc = new Scanner(System.in);  //Creamos un objetos de la clase Scanner, llamado sc 
+      Scanner sc = new Scanner(System.in); // Creamos un objetos de la clase Scanner, llamado sc
 
-      // do {
-      //   numero = sc.nextInt();
-      //   if (numero >= 0) {
-      //     while (numero < 10) {
-      //       numero *= 10;
-      //     }
-      //     sumaNumero += numero;
-      //   }
-      // } while (numero >= 0);
-      // System.out.println(sumaNumero);
-      
-      //Introducir la sucecion de numeros hasta la llegada de uno negativo no consegui que fueran separados
-      do {   
+      //Introduccion de los digitos.
+      do {
         numeroIntroducido = sc.nextInt();
         if (numeroIntroducido >= 0) {
           copiaNumero += (numeroIntroducido * 10);
         }
       } while (numeroIntroducido >= 0);
 
-      sc.close(); //Cierre del Scanner 
+      sc.close(); // Cierre del Scanner
 
-      //Voltear el numero
-      while (copiaNumero > 0) {   
-        numeroVolteado =  ((numeroVolteado * 10) + (copiaNumero % 10));
+      // Voltear el numero
+      while (copiaNumero > 0) {
+        numeroVolteado = ((numeroVolteado * 10) + (copiaNumero % 10));
         copiaNumero /= 10;
         if (copiaNumero != 0) {
           numeroDigitos++;
         }
       }
-      
-      copiaNumeroDigitos += numeroDigitos;
 
-      //Dibujar
-      for (int i = 0; i < numeroDigitos; i++){
+      copiaNumeroDigitos += numeroDigitos; // Para poder saber cual es el ultimo numero
+
+      // Dibujar
+      for (int i = 0; i < numeroDigitos; i++) {
         digitos = numeroVolteado % 10;
         numeroVolteado /= 10;
 
         // ---
-        for (int j = 0; j < digitos + 1; j++){
+        for (int j = 0; j < digitos + 1; j++) {
           System.out.print(" ---");
         }
-        System.out.println(""); //SALTO DE LINEA 
+        System.out.println(""); // SALTO DE LINEA
 
-        System.out.print("| " + digitos + " |"); //Escribir el numero
+        System.out.print("| " + digitos + " |"); // Escribir el numero
 
-        // | * | 
-        for (int j = 0; j < digitos; j++){
+        // | * |
+        for (int j = 0; j < digitos; j++) {
           System.out.print(" * ");
           System.out.print("|");
         }
-        System.out.println(""); //SALTO DE LINEA 
 
-        if (copiaNumeroDigitos == 1){
-          for (int j = 0; j < digitos + 1; j++){
+        System.out.println(""); // SALTO DE LINEA
+
+        if (copiaNumeroDigitos == 1) {
+          for (int j = 0; j < digitos + 1; j++) {
             System.out.print(" ---");
           }
-          System.out.println(""); //SALTO DE LINEA 
+          System.out.println(""); // SALTO DE LINEA
         }
 
         copiaNumeroDigitos--;
       }
-  System.out.println(""); //SALTO DE LINEA 
+      System.out.println(""); // SALTO DE LINEA
 
     } catch (Exception e) {
+      //Exception general por si se produce algun error.
       System.out.println(""); // SALTO DE LINEA
-      System.out.println("Error de mensaje: " + e.getMessage());
-      System.out.println("Clase de excepcion: " + e.getClass());
+      System.out.println("Mensaje de error: " + e.getMessage());
+      System.out.println("Clase de la excepcion: " + e.getClass());
       System.out.println(""); // SALTO DE LINEA
     }
   }
