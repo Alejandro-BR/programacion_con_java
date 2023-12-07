9f49a4881553e548d5833fbf67bc00557ff1ac64
Alejandro Barrionuevo Rosado <barrionuevorosadoalejandro@gmail.com>
2023-12-07 14:15:56 +0100

    Ejercicio 44 tema 5 bastante mas avanzado

--- 05_Bucles/ejercicios/T05Ejercico44.java
+++ 05_Bucles/ejercicios/T05Ejercico44.java
@@ -15,14 +15,66 @@
  * 
  * @author Alejandro Barrionuevo Rosado
  */
+import java.util.Scanner; 
 public class T05Ejercico44 {
   public static void main(String[] args) {
     try {
-      
+      long numeroVolteado = 0;
+      int numeroDigitos = 0;
+      int contador = 0;
+
+      Scanner sc = new Scanner(System.in);  //Creamos un objetos de la clase Scanner, llamado sc 
+
+      System.out.println(""); //SALTO DE LINEA 
+
+      System.out.print("Por favor, introduzca un número entero positivo: ");
+      long numero = sc.nextLong();
+
+      System.out.print("Introduzca la posición donde quiere insertar: ");
+      int posicion = sc.nextInt();
+
+      System.out.print("Introduzca el dígito que quiere insertar: ");
+      int digitoInsertado = sc.nextInt();
+
+      sc.close(); //Cierre del Scanner 
+
+      // Voltear el numero y sacar los digitos hasta el indicado
+      long copiaNumero = numero;
+
+      while (numeroDigitos <= posicion) {
+        numeroVolteado = ((numeroVolteado * 10) + (copiaNumero % 10));
+        copiaNumero /= 10;
+        numeroDigitos++;
+      }
+
+      // System.out.println(copiaNumero); //AYUDA
+
+      // La segunda parte del numero
+      long numeroAuxiliar = numero;
+      long numeroUltimaParte = copiaNumero;
+
+      while (numeroAuxiliar != 0) {
+        numeroAuxiliar /= 10;
+        contador++;
+      }
+
+      // System.out.println(contador); //AYUDA
+
+      for (int i = 0; i < (contador - numeroDigitos - 1); i++) {
+        numeroUltimaParte *= 10;
+      }
+
+      long resta = numero - numeroUltimaParte;
+      System.out.println(resta); //AYUDA
+
+      System.out.print("El número resultante es " + copiaNumero);
+      System.out.print(digitoInsertado);
+      System.out.print(resta + ".");
+
     } catch (Exception e) {
       System.out.println(""); //SALTO DE LINEA 
-      System.out.println("Error de mensaje: " + e.getMessage());
-      System.out.println("Clase de excepcion: " + e.getClass());
+      System.out.println("Mensaje de error: " + e.getMessage());
+      System.out.println("Clase de la excepcion: " + e.getClass());
       System.out.println(""); //SALTO DE LINEA 
     }
   }
