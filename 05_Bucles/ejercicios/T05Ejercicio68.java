ac1953a5bb39aaf77c3f677c37baf1eb6fe3f858
Alejandro Barrionuevo Rosado <barrionuevorosadoalejandro@gmail.com>
2023-12-10 21:52:58 +0100

    Ejercicio 68 tema 5

--- 05_Bucles/ejercicios/T05Ejercicio68.java
+++ 05_Bucles/ejercicios/T05Ejercicio68.java
@@ -19,12 +19,62 @@
  * 
  * @author Alejandro Barrionuevo Rosado
  */
-import java.util.Scanner; 
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class T05Ejercicio68 {
   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);  //Creamos un objetos de la clase Scanner, llamado sc 
-    System.out.println("Por favor, introduzca un número: ");
-    int numeroIntroducido = sc.nextInt();
-    sc.close(); //Cierre del Scanner 
+    try {
+      long numeroAuxiliar;
+      long copiaNumero;
+      long digito;
+      long numeroVolteado = 0;
+      int contador = 0;
+
+      Scanner sc = new Scanner(System.in); // Creamos un objetos de la clase Scanner, llamado sc
+
+      System.out.println(""); // SALTO DE LINEA
+      System.out.print("Por favor, introduzca un número: ");
+      int numeroIntroducido = sc.nextInt();
+
+      sc.close(); // Cierre del Scanner
+
+      // Voltear numero
+      copiaNumero = numeroIntroducido;
+      while (copiaNumero > 0) {
+        numeroVolteado = ((numeroVolteado * 10) + (copiaNumero % 10));
+        copiaNumero /= 10;
+        contador++;
+      }
+
+      System.out.print("Dislocando el " + numeroIntroducido + " sale el ");
+
+      numeroAuxiliar = numeroVolteado;
+      for (int i = 0; i < contador; i++) {
+        digito = numeroAuxiliar;
+        digito %= 10;
+        numeroAuxiliar /= 10;
+        if (digito % 2 == 0) {
+          System.out.print(digito + 1);
+        } else {
+          System.out.print(digito - 1);
+        }
+      }
+
+      System.out.println(""); //SALTO DE LINEA 
+      System.out.println(""); //SALTO DE LINEA 
+
+    } catch (InputMismatchException ime) {
+      System.out.println(""); // SALTO DE LINEA
+      System.out.println("No se perimiten introducir caracteres o letras.");
+      System.out.println(""); // SALTO DE LINEA
+    } catch (Exception e) {
+      System.out.println(""); // SALTO DE LINEA
+      System.out.println("Mensaje de error: " + e.getMessage());
+      System.out.println("Clase de la excepcion: " + e.getClass());
+      System.out.println(""); // SALTO DE LINEA
+    }
+
   }
 }
