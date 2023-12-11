caf04a9a404c116bb3af83b4af273fe154892e38
Alejandro Barrionuevo Rosado <barrionuevorosadoalejandro@gmail.com>
2023-12-11 12:47:37 +0100

    Ejercicio 52 tema 5

--- 05_Bucles/ejercicios/T05Ejercicio52.java
+++ 05_Bucles/ejercicios/T05Ejercicio52.java
@@ -1,3 +1,4 @@
+
 /**
  * Ejercicio 52: Realiza un programa que sea capaz de desplazar todos los dígitos de un número
  * de derecha a izquierda una posición. El dígito de más a la izquierda, pasaría a
@@ -28,13 +29,61 @@
  */
 
 import java.util.InputMismatchException; //catch ime
-import java.util.Scanner; 
+import java.util.Scanner;
 
 public class T05Ejercicio52 {
   public static void main(String[] args) {
-    try {    
-      Scanner sc = new Scanner(System.in);  //Creamos un objetos de la clase Scanner, llamado sc 
-      sc.close(); //Cierre del Scanner 
+    try {
+      long copiaNumero;
+      long numeroVolteado = 0;
+      long numeroAuxiliar = 0;
+      long digito = 0;
+      int contador = 0;
+      int primerNumero = 0;
+
+      Scanner sc = new Scanner(System.in); // Creamos un objetos de la clase Scanner, llamado sc
+
+      System.out.println(""); // SALTO DE LINEA
+      System.out.print("Introduzca un número: ");
+      long numero = sc.nextLong();
+
+      sc.close(); // Cierre del Scanner
+
+      // Voltear numero
+      copiaNumero = numero;
+      while (copiaNumero > 0) {
+        numeroVolteado = ((numeroVolteado * 10) + (copiaNumero % 10));
+        copiaNumero /= 10;
+        contador++;
+      }
+
+      numeroAuxiliar = numeroVolteado;
+      for (int i = 0; i < contador; i++) {
+        digito = numeroAuxiliar;
+        digito %= 10;
+        numeroAuxiliar /= 10;
+        if (i == 0)
+        primerNumero = (int) digito;
+      }
+
+      System.out.print("El número resultado es ");
+
+      numeroAuxiliar = numeroVolteado;
+      for (int i = 0; i < contador; i++) {
+        digito = numeroAuxiliar;
+        digito %= 10;
+        numeroAuxiliar /= 10;
+        if ((i == 0) || ((digito == 0) && (i == 1))){
+          System.out.print("");
+        } else {
+          System.out.print(digito);
+        }
+      }
+
+      System.out.println(primerNumero);
+
+      System.out.println(""); // SALTO DE LINEA
+
     } catch (InputMismatchException ime) {
       System.out.println(""); // SALTO DE LINEA
       System.out.println("No se perimiten introducir caracteres o letras.");
