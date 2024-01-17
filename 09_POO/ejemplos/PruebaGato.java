10cac8859aa8c3f4741375d70c1a1db6f8be1a4e
Alejandro Barrionuevo Rosado <barrionuevorosadoalejandro@gmail.com>
2024-01-17 21:20:05 +0100

    Ejemplo gatos terminar

--- 09_POO/ejemplos/PruebaGato.java
+++ 09_POO/ejemplos/PruebaGato.java
@@ -6,22 +6,21 @@
 public class PruebaGato {
   public static void main(String[] args) {
 
-    Gato garfield = new Gato("Garfield");
-    System.out.println(garfield.nombre);
-    System.out.println(garfield.color);
-    System.out.println(garfield.raza);
-    System.out.println(garfield.sexo);
-    System.out.println(garfield.edad);
-    System.out.println(garfield.peso);
+    // Gato garfield = new Gato("Garfield");
+    // System.out.println(garfield.nombre);
+    // System.out.println(garfield.color);
+    // System.out.println(garfield.raza);
+    // System.out.println(garfield.sexo);
+    // System.out.println(garfield.edad);
+    // System.out.println(garfield.peso);
 
-    Gato botas = new Gato("Botas", "Negro", "Egipcio", "Macho", 5, 4.2);
-    botas.edad = 6;
-    System.out.println(botas.nombre);
-    System.out.println(botas.color);
-    System.out.println(botas.raza);
-    System.out.println(botas.sexo);
-    System.out.println(botas.edad);
-    System.out.println(botas.peso);
+    // Gato botas = new Gato("Botas", "Negro", "Egipcio", "Macho", 5, 4.2);
+    // System.out.println(botas.nombre);
+    // System.out.println(botas.color);
+    // System.out.println(botas.raza);
+    // System.out.println(botas.sexo);
+    // System.out.println(botas.edad);
+    // System.out.println(botas.peso);
 
     // Gato missy = new Gato();
     // missy.sexo = "hembra";
@@ -31,5 +30,26 @@ public class PruebaGato {
     // garfield.ronronea();
     // garfield.peleaCon(botas);
     // botas.peleaCon(missy);
+
+    System.out.println("¿Cuantos gatos quieres crear?: ");
+    int num = Integer.parseInt(System.console().readLine());
+    Gato[] gaticos = new Gato[num];
+    String[] nombre = {"Garfield", "Botas", "Willy", "Pancho", "Misifu"};
+    String[] colores = {"Marron", "Negro", "Pardo"};
+    for (int i = 0; i < num; i++) {
+      int nombreAleatorios = (int)(Math.random()*5);
+      int coloresAleatorios = (int)(Math.random()*3);
+      System.out.println("¿Que raza deseas?: ");
+      String raza = System.console().readLine();
+      gaticos[i] = new Gato(nombre[nombreAleatorios], colores[coloresAleatorios], raza, "Macho", 0, 0);
+    }
+
+    System.out.println(); //Salto de linea
+
+    for (int i = 0; i < gaticos.length; i++) {
+      System.out.println(gaticos[i].nombre);
+      System.err.println(gaticos[i].color);
+      System.out.println();
+    }
   }
 }
