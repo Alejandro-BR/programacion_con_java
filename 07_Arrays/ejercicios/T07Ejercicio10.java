c07e34ff0a599969c04b0386bb8d5ad5337a8731
Alejandro Barrionuevo Rosado <barrionuevorosadoalejandro@gmail.com>
2024-01-20 17:22:55 +0100

    Ejercicio 11 tema 7

--- 07_Arrays/ejercicios/T07Ejercicio10.java
+++ 07_Arrays/ejercicios/T07Ejercicio10.java
@@ -4,8 +4,7 @@
  * y que los almacene en un array. El programa debe ser capaz de pasar todos
  * los números pares a las primeras posiciones del array (del 0 en adelante) y
  * todos los números impares a las celdas restantes. Utiliza arrays auxiliares
- * si es
- * necesario.
+ * si es necesario.
  * 
  * @author Alejandro Barrionuevo Rosado
  */
@@ -20,9 +19,9 @@ public class T07Ejercicio10 {
       int par[] = new int[20];
       int aux;
 
-      //Crear la array
+      // Crear la array
       for (int i = 0; i < numero.length; i++) {
-        aux = (int)(Math.random()*101);
+        aux = (int) (Math.random() * 101);
         if (aux % 2 == 0) {
           par[i] = aux;
         } else {
@@ -62,3 +61,47 @@ public class T07Ejercicio10 {
     }
   }
 }
+
+///////// Solucion del libro de Luis
+
+// int[] n = new int[20];
+// int[] par = new int[20];
+// int[] impar = new int[20];
+// int i;
+// int pares = 0;
+// int impares = 0;
+
+// for (i = 0; i < 20; i++) {
+// n[i] = (int)(Math.random() * 101);
+// // Separa los números metiendo los pares en un array
+// // y los impares en otro.
+// if (n[i] % 2 == 0) {
+// par[pares++] = n[i];
+// } else {
+// impar[impares++] = n[i];
+// }
+// }
+
+// // Muestra el array original
+// System.out.println("Array original:");
+// for (i = 0; i < 20; i++) {
+// System.out.print(n[i] + " ");
+// }
+// System.out.println();
+
+// // Mete los pares en las primeras posiciones
+// // del array original.
+// for (i = 0; i < pares; i++) {
+// n[i] = par[i];
+// }
+
+// // Mete los impares en los huecos que quedan.
+// for (i = pares; i < 20; i++) {
+// n[i] = impar[i - pares];
+// }
+
+// // Muestra el resultado.
+// System.out.println("Array con los pares al principio:");
+// for (i = 0; i < 20; i++) {
+// System.out.print(n[i] + " ");
+// }
---
+++ 07_Arrays/ejercicios/T07Ejercicio11.java
@@ -0,0 +1,80 @@
+/**
+ * Ejercicio 11: Realiza un programa que pida 10 números por teclado y que los
+ * almacene en
+ * un array. A continuación se mostrará el contenido de ese array junto al
+ * índice
+ * (0 – 9) utilizando para ello una tabla. Seguidamente el programa pasará los
+ * primos a las primeras posiciones, desplazando el resto de números (los que no
+ * son primos) de tal forma que no se pierda ninguno. Al final se debe mostrar
+ * el array resultante.
+ *
+ * Se encuentra un ejemplo en el libro.
+ * 
+ * @author Alejandro Barrionuevo Rosado
+ */
+
+public class T07Ejercicio11 {
+  public static void main(String[] args) {
+    int numeros[] = new int[10];
+    int numeroFinales[] = new int[10];
+    int num;
+    int indiceF = 0;
+
+    // Pedir y almacenar los 10 numeros
+    System.out.println();
+    for (int i = 0; i < numeros.length; i++) {
+      System.out.print("Introduce un numero entero: ");
+      num = Integer.parseInt(System.console().readLine());
+      numeros[i] = num;
+    }
+    System.out.println();
+
+    // Dibujar la array junto a su indice y averiguamos si es primo o no para
+    // guardarlo en otra array
+    System.out.println("Indice:  0   1   2   3   4   5   6   7   8   9");
+    System.out.print("Valor:");
+    for (int i = 0; i < numeros.length; i++) {
+      num = numeros[i];
+      System.out.printf("%4d", num);
+      // Si num es 2 o 0 como no entrari en mi bucle se le asignaria a numeroFinales
+      // desde aqui
+      if ((num == 2) || (num == 0)) {
+        numeroFinales[indiceF] = num;
+        indiceF++;
+        numeros[i] = 0;
+      }
+      // Bucle para asignar a numeroFinalesSSS
+      for (int j = 2; j < num; j++) {
+        if (num % j != 0) {
+          break;
+        } else {
+          numeroFinales[indiceF] = num;
+          indiceF++;
+          numeros[i] = 0;
+          break;
+        }
+      }
+    }
+
+    // Poner en la array final el resto de numeros
+    for (int i = 0; i < numeros.length; i++) {
+      if (numeros[i] != 0) {
+        numeroFinales[indiceF] = numeros[i];
+        indiceF++;
+      }
+    }
+
+    //Dibujar la array final
+    System.out.println();
+    System.out.println();
+    System.out.println("Indice:  0   1   2   3   4   5   6   7   8   9");
+    System.out.print("Valor:");
+    num = 0;
+    for (int i = 0; i < numeroFinales.length; i++) {
+      num = numeroFinales[i];
+      System.out.printf("%4d", num);
+    }
+
+    System.out.println();
+  }
+}
