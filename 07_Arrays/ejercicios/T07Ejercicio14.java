60ffdfbeb225502ba6e0e6565213fe786ad623d5
Alejandro Barrionuevo Rosado <barrionuevorosadoalejandro@gmail.com>
2024-01-21 17:22:31 +0100

    Ejercicio 14 tema 7 terminado

--- 07_Arrays/ejercicios/T07Ejercicio14.java
+++ 07_Arrays/ejercicios/T07Ejercicio14.java
@@ -12,15 +12,15 @@
  * 
  * Array original:
  * ┌────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┐
- * │ 0 │ 1 │ 2 │ 3 │ 4 │ 5 │ 6 │ 7 │
+ * │      0 │      1 │      2 │      3 │      4 │      5 │      6 │      7 │
  * ├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
- * │ casa │ azul │ verde │ orden │ morado │bombilla│ bici │ rosa │
+ * │   casa │   azul │  verde │  orden │ morado │bombilla│   bici │   rosa │
  * └────────┴────────┴────────┴────────┴────────┴────────┴────────┴────────┘
  * Array resultado:
  * ┌────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┐
- * │ 0 │ 1 │ 2 │ 3 │ 4 │ 5 │ 6 │ 7 │
+ * │      0 │      1 │      2 │      3 │      4 │      5 │      6 │      7 │
  * ├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
- * │ azul │ verde │ morado │ rosa │ casa │ orden │bombilla│ bici │
+ * │   azul │  verde │ morado │   rosa │   casa │  orden │bombilla│   bici │
  * └────────┴────────┴────────┴────────┴────────┴────────┴────────┴────────┘
  * 
  * @author Alejandro Barrionuevo Rosado
@@ -59,7 +59,7 @@ public class T07Ejercicio14 {
       }
     }
 
-    // Pintamos la array original
+    // Pintamos la array original **********************************************
     System.out.println();
     System.out.println("Array original:");
     System.out.println();
@@ -143,94 +143,113 @@ public class T07Ejercicio14 {
 
     System.out.println();
 
+    // Pintamos la array final **********************************************
+    System.out.println();
+    System.out.println("Array final:");
+    System.out.println();
 
+    // Primera linea de la tabla
 
+    for (int i = 0; i < colores.length; i++) {
+      if (i == 0) {
+        System.out.print("┌");
+      }
 
+      // Centro
+      for (int j = 0; j < maximosDigitos + 2; j++) {
+        System.out.print("─");
+      }
 
+      if (i == (palabras.length - 1)) {
+        System.out.println("┐");
+      } else {
+        System.out.print("┬");
+      }
+    }
 
-        // Pintamos la array final
-        System.out.println();
-        System.out.println("Array original:");
-        System.out.println();
-    
-        // Primera linea de la tabla
-    
-        for (int i = 0; i < colores.length; i++) {
-          if (i == 0) {
-            System.out.print("┌");
-          }
-    
-          // Centro
-          for (int j = 0; j < maximosDigitos + 2; j++) {
-            System.out.print("─");
-          }
-    
-          if (i == (palabras.length - 1)) {
-            System.out.println("┐");
-          } else {
-            System.out.print("┬");
-          }
-        }
-    
-        // Indice de la tabla
-    
-        for (int i = 0; i < colores.length; i++) {
-          System.out.print("│ " + i);
-          for (int j = 0; j < maximosDigitos; j++) {
+    // Indice de la tabla
+
+    for (int i = 0; i < colores.length; i++) {
+      System.out.print("│ " + i);
+      for (int j = 0; j < maximosDigitos; j++) {
+        System.out.print(" ");
+      }
+    }
+    System.out.println("│");
+
+    // Segunda linea de la tabla
+
+    for (int i = 0; i < colores.length; i++) {
+      if (i == 0) {
+        System.out.print("├");
+      }
+
+      // Centro
+      for (int j = 0; j < maximosDigitos + 2; j++) {
+        System.out.print("─");
+      }
+
+      if (i == (colores.length - 1)) {
+        System.out.println("┤");
+      } else {
+        System.out.print("┼");
+      }
+    }
+
+    // Palabras de la tabla
+
+    // Primero los colores
+    for (int i = 0; i < indiceColores; i++) {
+      System.out.print("│ " + colores[i]);
+      for (int j = 0; j <= (maximosDigitos - colores[i].length()); j++) {
+        System.out.print(" ");
+      }
+    }
+
+    // Las palabras menos los colores
+    for (int i = 0; i < palabras.length; i++) {
+      switch (palabras[i]) {
+        case "rojo":
+        case "verde":
+        case "azul":
+        case "amarillo":
+        case "naranja":
+        case "rosa":
+        case "negro":
+        case "blanco":
+        case "morado":
+          break;
+        default:
+          System.out.print("│ " + palabras[i]);
+          for (int j = 0; j <= (maximosDigitos - palabras[i].length()); j++) {
             System.out.print(" ");
           }
-        }
-        System.out.println("│");
-    
-        // Segunda linea de la tabla
-    
-        for (int i = 0; i < colores.length; i++) {
-          if (i == 0) {
-            System.out.print("├");
-          }
-    
-          // Centro
-          for (int j = 0; j < maximosDigitos + 2; j++) {
-            System.out.print("─");
-          }
-    
-          if (i == (colores.length - 1)) {
-            System.out.println("┤");
-          } else {
-            System.out.print("┼");
-          }
-        }
-    
-        // Palabras de la tabla
-    
-        for (int i = 0; i < colores.length; i++) {
-          System.out.print("│ " + colores[i]);
-          for (int j = 0; j <= (maximosDigitos - colores[i].length()); j++) {
-            System.out.print(" ");
-          }
-        }
-        System.out.println("│");
-    
-        // Ultima linea de la tabla
-    
-        for (int i = 0; i < colores.length; i++) {
-          if (i == 0) {
-            System.out.print("└");
-          }
-    
-          // Centro
-          for (int j = 0; j < maximosDigitos + 2; j++) {
-            System.out.print("─");
-          }
-    
-          if (i == (colores.length - 1)) {
-            System.out.println("┘");
-          } else {
-            System.out.print("┴");
-          }
-        }
-    
-        System.out.println();
+          break;
+      }
+    }
+
+    System.out.println("│");
+
+    // Ultima linea de la tabla
+
+    for (int i = 0; i < colores.length; i++) {
+      if (i == 0) {
+        System.out.print("└");
+      }
+
+      // Centro
+      for (int j = 0; j < maximosDigitos + 2; j++) {
+        System.out.print("─");
+      }
+
+      if (i == (colores.length - 1)) {
+        System.out.println("┘");
+      } else {
+        System.out.print("┴");
+      }
+    }
+
+    System.out.println();
 
   }
 }
