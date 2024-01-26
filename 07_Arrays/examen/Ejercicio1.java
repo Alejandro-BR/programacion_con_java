f58721685a0437b21da03f56a4a6b68f318f6b02
Alejandro Barrionuevo Rosado <barrionuevorosadoalejandro@gmail.com>
2024-01-26 17:34:11 +0100

    Examen tema 7

---
+++ 07_Arrays/examen/Ejercicio1.java
@@ -0,0 +1,86 @@
+/**
+ * Ejercicio 1 Examen
+ * 
+ * @author Alejandro Barrionuevo Rosado
+ */
+
+public class Ejercicio1 {
+  public static void main(String[] args) {
+    int maximoIntervalo = 0;
+    int minimoIntervalo = 0;
+    int resultado;
+    int intentosUsados = 0;
+    ;
+    boolean ganar = false;
+    int numeroIntroducido;
+    int opcion = 0;
+
+    do {
+      // Salida por pantalla y recoger datos
+      System.out.println();
+      System.out.println("-D-I-V-I-N-A-R-E-M-");
+      System.out.println("Divinarem pensará un número entre los valores que tu decidas");
+      System.out.println();
+      System.out.print("Introduzca el minimo valor del intervalo: ");
+      int minimo = Integer.parseInt(System.console().readLine());
+      System.out.print("Introduzca el maximo valor del intervalo: ");
+      int maximo = Integer.parseInt(System.console().readLine());
+      System.out.println("-D-I-V-I-N-A-R-E-M-");
+
+      // Creacion del intervalo
+      do {
+        maximoIntervalo = (int) (Math.random() * (maximo - minimo + 1)) + minimo;
+        minimoIntervalo = (int) (Math.random() * (maximo - minimo + 1)) + minimo;
+      } while ((minimoIntervalo >= maximoIntervalo));
+
+      System.out.println("Estoy pensando en un numero entre " + minimoIntervalo + " y " + maximoIntervalo);
+      System.out.println();
+
+      // Calculo de los intentos
+      int intentos = (int) (Math.log(maximoIntervalo - minimoIntervalo + 1) / Math.log(2));
+      System.out.println("Tines " + intentos + " intentos");
+      System.out.println();
+
+      // Creacion del numero que buscamos
+      resultado = (int) (Math.random() * (maximoIntervalo - minimoIntervalo + 1)) + minimoIntervalo;
+
+      // Buscamos el numero
+      do {
+        System.out.print("Escribe el numero: ");
+        numeroIntroducido = Integer.parseInt(System.console().readLine());
+        if (numeroIntroducido > resultado) {
+          System.out.println("Te has pasado");
+        } else if (numeroIntroducido < resultado) {
+          System.out.println("Te has quedado corto");
+        } else if (numeroIntroducido == resultado) {
+          ganar = true;
+        }
+        intentosUsados++;
+      } while ((intentosUsados < intentos) && (ganar == false));
+
+      System.out.println();
+
+      // Final de la partida
+      if (ganar == true) {
+        System.out.println("Enhorabuena!!!! has acertado.");
+        System.out.println("Efectivamente el numero era: " + resultado);
+      } else {
+        System.out.println("Has superado el numero de intentos.");
+        System.out.println("El numero que habia pensado era: " + resultado);
+      }
+
+      System.out.println();
+
+      // Menu para empezar otra partida o salir
+      System.out.println("Elige una opcion");
+      System.out.println("1. Volver a jugar");
+      System.out.println("2. Salir del juego");
+      System.out.print("Opcion: ");
+      opcion = Integer.parseInt(System.console().readLine());
+    } while (opcion == 1);
+
+    System.out.println();
+    System.out.println("Gracias por jugar -D-I-V-I-N-A-R-E-M-");
+    System.out.println();
+  }
+}
---
+++ 07_Arrays/examen/Ejercicio2.java
@@ -0,0 +1,228 @@
+/**
+ * Ejercicio 2 Examen
+ * 
+ * @author Alejandro Barrionuevo Rosado
+ */
+
+public class Ejercicio2 {
+
+  /**
+   * Función para generar una tabla con números aleatorios
+   * 
+   * @param filas        int
+   * @param columnas     int
+   * @param maxAleatorio int
+   * @return int[][]
+   */
+  public static int[][] generarTablaAleatoria(int filas, int columnas, int maxAleatorio) {
+    int[][] tabla = new int[filas][columnas];
+    for (int i = 0; i < tabla.length; i++) {
+      for (int j = 0; j < tabla[i].length; j++) {
+        tabla[i][j] = (int) (Math.random() * maxAleatorio + 1); // +1 Para incluir al numero
+      }
+    }
+    return tabla;
+  }
+
+  /**
+   * Función para generar una tabla introduciendp números manualmente
+   * 
+   * @param filas    int
+   * @param columnas int
+   * @return int[][]
+   */
+  public static int[][] generarTablaManual(int filas, int columnas) {
+    int[][] tabla = new int[filas][columnas];
+    for (int i = 0; i < tabla.length; i++) {
+      for (int j = 0; j < tabla[i].length; j++) {
+        System.out.print("Introduce el numero: ");
+        int numero = Integer.parseInt(System.console().readLine());
+        tabla[i][j] = numero;
+      }
+    }
+    return tabla;
+  }
+
+  /**
+   * Función para mostrar la tabla
+   * 
+   * @param tabla int[][]
+   */
+  public static void mostrarTabla(int[][] tabla) {
+    // Si no hay tabla que mande un mensaje de error
+    if (tabla == null) {
+      System.out.println("Primero debe generar la tabla.");
+      return;
+    }
+    // Mostrar
+    for (int i = 0; i < tabla.length; i++) {
+      for (int j = 0; j < tabla[i].length; j++) {
+        System.out.printf("%-4d", tabla[i][j]);
+      }
+      System.out.println();
+    }
+  }
+
+  /**
+   * Función para intercambiar dos filas en la tabla
+   * 
+   * @param tabla int[][]
+   * @param fila1 int
+   * @param fila2 int
+   */
+  public static void intercambiarFilas(int[][] tabla, int fila1, int fila2) {
+    int[] aux = new int[tabla[fila1].length]; // Creamos una variable auxiliar que sera una array
+    // Copiamos en el aux
+    for (int i = 0; i < aux.length; i++) {
+      aux[i] = tabla[fila1][i];
+    }
+    // Intercambiar
+    for (int i = 0; i < aux.length; i++) {
+      tabla[fila1][i] = tabla[fila2][i]; // fila1 = fila2
+      tabla[fila2][i] = aux[i]; // fila2 = aux
+    }
+  }
+
+  /**
+   * Función para intercambiar dos columnas en la tabla
+   * 
+   * @param tabla int[][]
+   * @param col1  int
+   * @param col2  int
+   */
+  public static void intercambiarColumnas(int[][] tabla, int col1, int col2) {
+    int[] aux = new int[tabla.length]; // Creamos una variable auxiliar que sera una array
+    // Copiamos en el aux
+    for (int i = 0; i < aux.length; i++) {
+      aux[i] = tabla[i][col1];
+    }
+    // Intercambiar
+    for (int i = 0; i < aux.length; i++) {
+      tabla[i][col1] = tabla[i][col2]; // col1 = col2
+      tabla[i][col2] = aux[i]; // col2 = aux
+    }
+  }
+
+  /**
+   * Función para sumar los elementos de una fila
+   * 
+   * @param tabla int[][]
+   * @param fila  int
+   * @return int
+   */
+  public static int sumarFila(int[][] tabla, int fila) {
+    int suma = 0;
+    for (int i = 0; i < tabla.length; i++) {
+      suma += tabla[fila][i];  //No se mueve la fila
+    }
+    return suma;
+  }
+
+  /**
+   * Función para sumar los elementos de una columna
+   * 
+   * @param tabla int[][]
+   * @param col int
+   * @return int
+   */
+  public static int sumarColumna(int[][] tabla, int col) {
+    int suma = 0;
+    for (int i = 0; i < tabla.length; i++) {
+      suma += tabla[i][col]; //No se mueve la columna
+    }
+    return suma;
+  }
+
+  public static void main(String[] args) {
+    int[][] tabla = null;
+    boolean continuar = true;
+    int filas;
+    int columnas;
+    int fila1;
+    int col1;
+    int fila2;
+    int col2;
+    int filaSuma;
+    int colSuma;
+
+    while (continuar) {
+      System.out.println("\n--- Menú ---");
+      System.out.println("1. Generar tabla aleatoriamente");
+      System.out.println("2. Generar tabla manualmente");
+      System.out.println("3. Mostrar tabla");
+      System.out.println("4. Intercambiar dos filas");
+      System.out.println("5. Intercambiar dos columnas");
+      System.out.println("6. Sumar elementos de una fila");
+      System.out.println("7. Sumar elementos de una columna");
+      System.out.println("8. Salir");
+
+      System.out.print("Seleccione una opción: ");
+      int opcion = Integer.parseInt(System.console().readLine());
+
+      switch (opcion) {
+        case 1: // Generar tabla en modo Aleatorio
+          System.out.print("Introduce las filas que quieres: ");
+          filas = Integer.parseInt(System.console().readLine());
+          System.out.print("Introduce las columnas que quieres: ");
+          columnas = Integer.parseInt(System.console().readLine());
+          System.out.print("Introduce el maximo valor aleatorio que quieres: ");
+          int maxAleatorio = Integer.parseInt(System.console().readLine());
+          tabla = generarTablaAleatoria(filas, columnas, maxAleatorio); // LLAMAR FUNCION
+          System.out.println();
+          break;
+        case 2: // Generar tabla en modo Manual
+          System.out.print("Introduce las filas que quieres: ");
+          filas = Integer.parseInt(System.console().readLine());
+          System.out.print("Introduce las columnas que quieres: ");
+          columnas = Integer.parseInt(System.console().readLine());
+          tabla = generarTablaManual(filas, columnas); // LLAMAR FUNCION
+          System.out.println();
+          break;
+        case 3: // Mostrar la tabla
+          System.out.println();
+          mostrarTabla(tabla); // LLAMAR FUNCION
+          System.out.println();
+          break;
+        case 4: // Intercambiar dos filas
+          System.out.print("Introduce la primera fila: ");
+          fila1 = Integer.parseInt(System.console().readLine());
+          System.out.print("Introduce la segunda fila: ");
+          fila2 = Integer.parseInt(System.console().readLine());
+          intercambiarFilas(tabla, fila1, fila2); // LLAMAR FUNCION
+          System.out.println();
+          break;
+        case 5: // Intercambiar dos columnas
+          System.out.print("Introduce la primera fila: ");
+          col1 = Integer.parseInt(System.console().readLine());
+          System.out.print("Introduce la segunda fila: ");
+          col2 = Integer.parseInt(System.console().readLine());
+          intercambiarColumnas(tabla, col1, col2); // LLAMAR FUNCION
+          System.out.println();
+          break;
+        case 6: // Sumar una fila
+          System.out.print("Introduce la fila que quieres sumar: ");
+          filaSuma = Integer.parseInt(System.console().readLine());
+          int sumaFila = sumarFila(tabla, filaSuma); // LLAMAR FUNCION
+          System.out.println();
+          System.out.println("Suma: " + sumaFila);
+          System.out.println();
+          break;
+        case 7: // Sumar una columna
+          System.out.print("Introduce la columna que quieres sumar:");
+          colSuma = Integer.parseInt(System.console().readLine());
+          int sumaCol = sumarColumna(tabla, colSuma); // LLAMAR FUNCION
+          System.out.println();
+          System.out.println("Suma: " + sumaCol);
+          System.out.println();
+          break;
+        case 8: // Salir de la aplicacion
+          System.out.println("¡Gracias!");
+          System.out.println();
+          continuar = false;
+          break;
+        default:
+          System.out.println("Opción no válida. Por favor, seleccione una opción válida.");
+      }
+    }
+  }
+}
