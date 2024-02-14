9776afd00e231a993c3e9b3e68b64d6d6a7da891
Alejandro Barrionuevo Rosado <barrionuevorosadoalejandro@gmail.com>
2024-02-14 21:18:40 +0100

    Ejercicio 3 arrays tema 9 empezado

---
+++ 09_POO/ejercicios/Ejercicio_03_arrays/Coleccion.java
@@ -0,0 +1,17 @@
+/**
+ * Ejercicio 3: Realiza el programa “Colección de discos” por tu cuenta, mirando lo menos
+ * posible el ejemplo que se proporciona. Pruébalo primero para ver cómo
+ * funciona y luego intenta implementarlo tú mismo.
+ * 
+ * Aplicacion para organizar tu bibiloteca de discos
+ * 
+ * @author Alejandro Barrionuevo Rosado
+ */
+
+package Ejercicio_03_arrays;
+
+public class Coleccion {
+  public static void main(String[] args) {
+    
+  }
+}
---
+++ 09_POO/ejercicios/Ejercicio_03_arrays/Discos.java
@@ -0,0 +1,26 @@
+/**
+ * Clase Disco
+ * 
+ * @author Alejandro Barrionuevo Rosado
+ */
+
+package Ejercicio_03_arrays;
+
+public class Discos {
+  //////// Atributos
+  private String codigo;
+  private String nombre;
+  private String album;
+  private String artista;
+  private int horasDuracion;
+
+  //////// Metodos
+  /**
+   * Contructor de la clase
+   */
+  public Discos(){
+    this.codigo = "LIBRE";
+  }
+
+  
+}
